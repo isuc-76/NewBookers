27fa5aef88bf0b329df02e3a42307aa26b981120
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-05 20:38:57 +0900

    yabee

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,10 @@ class BooksController < ApplicationController
         @user = @book.user
     end
 
+    def user
+        @users = User.all
+    end
+
     def destroy
         @book = Book.find(params[:id])
         @book.destroy
