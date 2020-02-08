b5ca6cd9f6c6a95e470a0c1a4987858c854904e6
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-08 18:00:25 +0900

    gazou!

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'c31a34bd043cdbed977e3874bdc3e6da635f29334d517c0babf7075da0a94fa2105b49b883e5f23ca4cfeea2ce307ad9a67234b85838fe3aae059adaad86e702'
