70c5ee510a65d385019d9ffdbee8dc39c79e51ad
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-09 17:32:18 +0900

    ok?

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,21 @@ class BooksController < ApplicationController
         @user = @book.user
     end
 
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if
+        @book.update(book_params)
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+        else
+            render action: :edit
+        end
+    end
+
 
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
   resources :homes, only: [:index, :create, :show, :edit, :destroy]
-  resources :books, only: [:index, :create, :show, :edit, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :destroy, :update]
   resources :users, only: [:index, :create, :show, :edit, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
