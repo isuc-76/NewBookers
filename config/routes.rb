69f14a389a57f51faa8e41f105056ca389190eb7
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-11 18:17:39 +0900

    kinou ok!

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :edit, :destroy, :update]
   resources :abouts, only: [:index]
   root 'tops#index'
+
+  get '/home/about' => 'abouts#index', as: "about"
+#      メソッドの代入                        ↑action名の変更
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
